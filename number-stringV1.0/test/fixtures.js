f2f1b43c6da167a61be973b5839641cebf97edaf
crystalynliu <crystalynliu@163.com>
2016-03-01 16:29:49 +0800

    add function loadNumDictionary

--- number-stringV1.0/test/fixtures.js
+++ number-stringV1.0/test/fixtures.js
@@ -7,3 +7,16 @@ function loadUnits() {
   ];
 }
 
+function loadNumDictionary(){
+  return {
+    unit:[
+      "","one","two","three","four","five","six","seven","eight","nine",
+      "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen",
+      "seventeen","eighteen","nineteen"
+    ]
+    decade:[
+      "","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"
+    ]
+    hundred:"hundred"
+  }
+}
