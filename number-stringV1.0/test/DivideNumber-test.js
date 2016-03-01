3713400759035cf99842ce8dedf7cf62c3d14875
crystalynliu <crystalynliu@163.com>
2016-03-01 16:47:18 +0800

    add content to DivideNumber-test

---
+++ number-stringV1.0/test/DivideNumber-test.js
@@ -0,0 +1,38 @@
+describe('Test the function divideNumber', function() {
+  
+  it('divide one bit number', function() {
+    var result = divideNumber("1");
+    expect(result).toEqual(["1"]);
+  });
+
+  it('divide two bit number', function() {
+    var result = divideNumber("12");
+    expect(result).toEqual(["12"]);
+  });
+
+  it('divide three bit number', function() {
+    var result = divideNumber("123");
+    expect(result).toEqual(["123"]);
+  });
+
+  it('divide four bit number', function() {
+    var result = divideNumber("1234");
+    expect(result).toEqual(["234","1"]);
+  });
+
+  it('divide five bit number', function() {
+    var result = divideNumber("12345");
+    expect(result).toEqual(["345","12"]);
+  });
+
+  it('divide seven bit number', function() {
+    var result = divideNumber("1234567");
+    expect(result).toEqual(["7","456","123"]);
+  });
+
+  it('divide ten bit number', function() {
+    var result = divideNumber("1234567890");
+    expect(result).toEqual(["0","789","456","123"]);
+  });
+
+});
