320bb36a329e18e746f0191fe480be92c16ccb4a
crystalynliu <crystalynliu@163.com>
2016-03-01 16:51:33 +0800

    modify a bug in fixtures.js

--- number-stringV1.0/test/fixtures.js
+++ number-stringV1.0/test/fixtures.js
@@ -13,10 +13,10 @@ function loadNumDictionary(){
       "","one","two","three","four","five","six","seven","eight","nine",
       "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen",
       "seventeen","eighteen","nineteen"
-    ]
+    ],
     decade:[
       "","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"
-    ]
+    ],
     hundred:"hundred"
   }
 }
