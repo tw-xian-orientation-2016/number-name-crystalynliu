20e4fcb0c7313ebcd22bcdd9b6b495fcc0d030c4
crystalynliu <crystalynliu@163.com>
2016-03-01 21:29:14 +0800

    modify the value name in MergeStrings-test.js

--- number-stringV1.0/test/MergeStrings-test.js
+++ number-stringV1.0/test/MergeStrings-test.js
@@ -1,43 +1,43 @@
 describe('Test the function mergeStrings', function() {
-  var Connector = loadConnector();
+  var c = loadconnector();
 
   it('Merge one string', function() {
-    var result = mergeStrings(["one hundred and one"],Connector);
+    var result = mergeStrings(["one hundred and one"],connector);
     expect(result).toEqual(["one hundred and one"]);
   });
 
   it('Merge two string', function() {
-    var result = mergeStrings(["one hundred and one","twenty thousand"],Connector);
+    var result = mergeStrings(["one hundred and one","twenty thousand"],connector);
     expect(result).toEqual("twenty thousand,one hundred and one");
   });
 
   it('Merge two string', function() {
-    var result = mergeStrings(["one","twenty thousand"],Connector);
+    var result = mergeStrings(["one","twenty thousand"],connector);
     expect(result).toEqual("twenty thousand and one");
   });
 
   it('Merge two string', function() {
-    var result = mergeStrings(["one","three hundred and eleven thousand"],Connector);
+    var result = mergeStrings(["one","three hundred and eleven thousand"],connector);
     expect(result).toEqual("three hundred and eleven thousand and one");
   });
 
   it('Merge three string', function() {
-    var result = mergeStrings(["one hundred and one","two hundred and eleven thousand","twenty"],Connector);
+    var result = mergeStrings(["one hundred and one","two hundred and eleven thousand","twenty"],connector);
     expect(result).toEqual("twenty million,two hundred and eleven thousand,one hundred and one");
   });
 
   it('Merge three string', function() {
-    var result = mergeStrings(["one","two hundred and eleven thousand","twenty"],Connector);
+    var result = mergeStrings(["one","two hundred and eleven thousand","twenty"],connector);
     expect(result).toEqual("twenty million,two hundred and eleven thousand and one");
   });
 
   it('Merge three string', function() {
-    var result = mergeStrings(["one hundred and one","eleven thousand","twenty"],Connector);
+    var result = mergeStrings(["one hundred and one","eleven thousand","twenty"],connector);
     expect(result).toEqual("twenty million and eleven thousand,one hundred and one");
   });
 
   it('Merge three string', function() {
-    var result = mergeStrings(["one","eleven thousand","twenty"],Connector);
+    var result = mergeStrings(["one","eleven thousand","twenty"],connector);
     expect(result).toEqual("twenty million and eleven thousand and one");
   });
   
