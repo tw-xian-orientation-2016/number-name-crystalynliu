cb07b84f838f6ce7c5d5aa8acefbcb9593258c35
crystalynliu <crystalynliu@163.com>
2016-03-01 20:38:48 +0800

    add function CovertToText and pass the test

--- number-stringV1.0/main/main.js
+++ number-stringV1.0/main/main.js
@@ -13,3 +13,57 @@ function divideNumber(number) {
   return NumberArray;
 }
 
+function covertToText(NumberArray,numberDictionary){
+  var stringArray = [];
+  NumberArray.forEach(function(number){
+    var subString = "";
+    var length = number.length;
+    switch(length){
+      case 1:
+        subString = getOneBitString(number,numberDictionary);
+        break;
+      case 2:
+        subString = getTwoBitString(number,numberDictionary);
+        break;
+      case 3:
+        subString = getThreeBitString(number,numberDictionary);
+        break;
+    }
+    stringArray.push(subString);
+  })
+  return stringArray;
+}
+
+function getOneBitString(number,numberDictionary){
+  return numberDictionary.unit[parseInt(number)];
+}
+
+function getTwoBitString(number,numberDictionary){
+  var decade = parseInt(number.charAt(0));
+  var unit = parseInt(number.charAt(1));
+
+  if(decade===0){
+    return numberDictionary.unit[unit];
+  }else if(decade===1){
+    return numberDictionary.unit[number];
+  }else{
+    var unitString = numberDictionary.unit[unit];
+    var decadeString = numberDictionary.decade[decade];
+    
+    return unitString===""?decadeString:decadeString+" "+unitString;
+    
+  }
+}
+
+function getThreeBitString(number,numberDictionary){
+  var firstString = getOneBitString(number.substring(0,1),numberDictionary);
+  if(firstString!==""){
+    firstString += " " + numberDictionary.hundred;
+  }
+
+  var subNumber = number.slice(-2);
+  var subString = getTwoBitString(subNumber,numberDictionary);
+  subString = subString===""||firstString===""?subString:" and "+subString;
+
+  return firstString + subString ;
+}
