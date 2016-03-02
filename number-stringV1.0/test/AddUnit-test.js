3d1cb23b25b24f1edc80d72b581a1672507ab1f4
crystalynliu <crystalynliu@163.com>
2016-03-02 14:38:50 +0800

    modify AddUnit-test.js

--- number-stringV1.0/test/AddUnit-test.js
+++ number-stringV1.0/test/AddUnit-test.js
@@ -1,27 +1,32 @@
-describe('Test the function AddUnit', function() {
+describe('addUnit()', function() {
   var Units = loadUnits();
 
-  it('Add unit to one string', function() {
+  it('correct result when have 1 string', function() {
     var result = addUnit(["one hundred and one"],Units);
     expect(result).toEqual(["one hundred and one"]);
   });
 
-  it('Add unit to two string', function() {
+  it('correct result when have 2 string', function() {
     var result = addUnit(["one hundred and one","twenty"],Units);
     expect(result).toEqual(["one hundred and one","twenty thousand"]);
   });
 
-  it('Add unit to three string', function() {
+  it('correct result when have 3 string && second is ""', function() {
     var result = addUnit(["one hundred and one","","twenty"],Units);
     expect(result).toEqual(["one hundred and one","","twenty million"]);
   });
 
-  it('Add unit to other three string', function() {
+  it('correct result when have 3 string && first and second both is ""', function() {
+    var result = addUnit(["","","twenty"],Units);
+    expect(result).toEqual(["","","twenty million"]);
+  });
+
+  it('correct result when have 3 string', function() {
     var result = addUnit(["one hundred and one","eleven","twenty"],Units);
     expect(result).toEqual(["one hundred and one","eleven thousand","twenty million"]);
   });
 
-  it('Add unit to four string', function() {
+  it('correct result when have 4 string', function() {
     var result = addUnit(["one hundred and one","eleven","one hundred and twenty","nine"],Units);
     expect(result).toEqual(["one hundred and one","eleven thousand"
       ,"one hundred and twenty million","nine billion"]);
